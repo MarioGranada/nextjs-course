f1c62bbd0e7ee62f2f4f021f85739c2ccebc6a4b
MarioGranada <mario.granada190@gmail.com>
2023-03-01 21:46:54 -0800

    compare links navigation startegies and next image component

--- pages/new-photos/index.js
+++ pages/new-photos/index.js
@@ -1,5 +1,15 @@
-const NewPhotosPage = ({photos}) => {
-    return <>in here photos page {photos?.length} </>;
+import Image from "next/image";
+
+function NewPhotosPage(){
+    return <>
+    <h1> Stock Photos in here</h1>
+    {[1,2,3,4,5].map(item => {
+        return <div style={{margin: '54px', border: "1px solid" }} key={item} >
+            {/* <img src={`/stock-photos/stock-photo-${item}.jpg`} alt={`photo-${item}`} width="280" height="420" /> */}
+            <Image src={`/stock-photos/stock-photo-${item}.jpg`} width="280" height="420"/>
+        </div> 
+    })}
+     </>;
 }
 
 export default NewPhotosPage;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,8 +1,38 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 const NewsPage = ({testData, title, description}) => {
+    const router = useRouter();
+    const routeWithoutReplace = () => {router.push('/users')};
+    const routeWithReplace = () => {router.replace('/users')};
     return <>
     <h1>{title}</h1>
       <p>{description}</p>
-    in here news index page {testData}</>
+    in here news index page {testData}
+    <div>
+    <hr/>
+    <h2>Link component vs. {"<a>"} tag </h2>
+        <Link href='/'>Home with Link component</Link>
+        <br/>
+        <a href='/'>Home with {"<a>"} tag</a>
+        <hr/>
+    </div>
+    <div>
+    <hr/>
+    <h2>Link component using replace prop </h2>
+        <Link href='/users'>Link component without replace</Link>
+        <br/>
+        <Link href="/users" replace>Link component with replace</Link>
+        <hr/>
+    </div>
+    <div>
+        <hr/>
+        <h2>Use router.push </h2>
+        <button onClick={routeWithoutReplace}>Router.push without replace</button>
+        <br/>
+        <button onClick={routeWithReplace}>Router.push with replace</button>
+    </div>
+    </>
 }
 
 export function getStaticProps(context) {
